7d733d1048721b78a950cef4a7df33efe8917330
Kyler Akers <kylera14@gmail.com>
2020-07-31 11:13:47 -0500

    Add project files.

---
+++ ScissorItem.cs
@@ -0,0 +1,50 @@
+﻿using R2API;
+using RoR2;
+
+namespace Scissors
+{
+    internal class ScissorItem
+    {
+        internal static ItemIndex index;
+
+        internal static void Init()
+        {
+            //LoadAssets();
+            AddTokens();
+            AddItem();
+        }
+
+        private static void AddTokens()
+        {
+            LanguageAPI.Add("SCISSORS_NAME_TOKEN", "Rusty Scissors");
+            LanguageAPI.Add("SCISSORS_PICK_TOKEN", "Converts defeated enemies into useful scrap material.");
+            LanguageAPI.Add("SCISSORS_DESC_TOKEN", "Defeated enemies generate scrap that is converted to random items.");
+            LanguageAPI.Add("SCISSORS_LORE_TOKEN", "Never underestimate the usefulness of a pair of handy survival scissors.");
+        }
+
+        private static void AddItem()
+        {
+            ItemDef def = new ItemDef
+            {
+                name = "RustyScissors",
+                nameToken = "SCISSORS_NAME_TOKEN",
+                pickupToken = "SCISSORS_PICK_TOKEN",
+                descriptionToken = "SCISSORS_DESC_TOKEN",
+                loreToken = "SCISSORS_LORE_TOKEN",
+                tier = ItemTier.Tier1,
+                pickupIconPath = null,
+                pickupModelPath = null,
+                canRemove = true,
+                hidden = false,
+                tags = new ItemTag[]
+                {
+                    ItemTag.Utility,
+                    ItemTag.OnKillEffect
+                }
+            };
+            ItemDisplayRule[] rules = new ItemDisplayRule[0];
+            CustomItem item = new CustomItem(def, rules);
+            index = ItemAPI.Add(item);
+        }
+    }
+}
---
+++ ScissorsPlugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using R2API.Utils;
+
+namespace Scissors
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [R2APISubmoduleDependency(new string[] { "ResourcesAPI", "LanguageAPI", "ItemAPI"})]
+    [BepInPlugin(modGuid, modName, modVer)]
+    public sealed class ScissorsPlugin : BaseUnityPlugin
+    {
+        private const string modName = "RustyScissors";
+        private const string modGuid = "com.RicoValdezio." + modName;
+        private const string modVer = "0.0.1";
+        internal static ScissorsPlugin instance;
+
+        private void Awake()
+        {
+            if (instance == null) instance = this;
+            ScissorItem.Init();
+        }
+    }
+}
